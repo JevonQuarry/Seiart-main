9e6dcbfc01133efa833510e4fb60d7b7ff6fdda3
rajitechis <rajit@techis.io>
2021-12-31 08:05:26 +0530

    changes

--- frontend/src/containers/Profile.jsx
+++ frontend/src/containers/Profile.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from 'react';
 import userImg from '../assets/icons/User.png';
-import addImg from '../assets/icons/Add.png';
-import image from '../assets/img/image_9.jpg';
+
 import { updateProfile } from '../reducks/users/operations';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -11,7 +10,7 @@ function Profile() {
     const dispatch = useDispatch();
     const selector = useSelector(state => state);
     const user = getUsers(selector);
-    const userValues = { name: user.user_name, email: user.email, profile: user.profile };
+    const userValues = { name: user.user_name };
     const [values, setValues] = useState(userValues);
     const [image, setImage] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
@@ -32,14 +31,7 @@ function Profile() {
         setValues({ ...values, profile: null });
     };
 
-    const handleInputChange = e => {
-        const { name, value } = e.target;
-
-        setValues({
-            ...values,
-            [name]: value
-        });
-    };
+  
 
     const updateProfileHandler = async () => {
         setIsLoading(true);
@@ -60,44 +52,15 @@ function Profile() {
                         className={`upload-area`}
                         alt="Upload"
                     />
-                    <div className="upload-text">{`${isLoading ? 'Updating Profile...' : 'Edit Profile'}`}</div>
-                    <label className="profile-input-label" htmlFor="name">
-                        Name
-                    </label>
-                    <input
-                        onChange={handleInputChange}
-                        type="text"
-                        value={values.name}
-                        name="name"
-                        className="profile-input"
-                        placeholder="Type your name"
-                    />
-                    <label className="profile-input-label" htmlFor="name">
-                        Mail Address
-                    </label>
-                    <input
-                        onChange={handleInputChange}
-                        type="email"
-                        value={values.email}
-                        name="email"
-                        className="profile-input"
-                        placeholder="Type your mail address"
-                    />
+                
                     <button onClick={updateProfileHandler} type="button" className="custom-btn">
                         Done
                     </button>
                 </form>
 
-                <h1 className="myprofile-name">UserName</h1>
+                
             </div>
-            <div className="feed">
-                <div className="new-post">
-                    <input className="newpost-icon" type="file" src={addImg} />
-                </div>
-                <div className="myart-body">
-                    <img className="myart-picture" src={image} alt="" />
-                </div>
-            </div>
+           
         </>
     );
 }
